781d3f2d70fb87389f64a122298abfefac4dcce8
Felipe Tomaz <felipetomaz2206@gmail.com>
2024-10-20 17:39:16 -0300

    wip: create page to show all recipes

--- easy_recipe/urls.py
+++ easy_recipe/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ingredients/', include('ingredients.urls'))
+    path('ingredients/', include('ingredients.urls')),
+    path('recipes/', include('recipes.urls'))
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Recipe
+
+class SeeAllRecipesView(generic.ListView):
+    context_object_name = 'recipe_list'
+    template_name = 'recipes/index.html'
+
+    def get_queryset(self):
+        return Recipe.objects.all()

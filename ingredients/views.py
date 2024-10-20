53234db1901545414e3a288c71985da6961f31ff
Felipe Tomaz <felipetomaz2206@gmail.com>
2024-10-20 13:18:01 -0300

    feat: implement page to present all ingredients

--- ingredients/views.py
+++ ingredients/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views import generic
+from django.db.models import Q as FilterParam
 
-# Create your views here.
+from .models import Ingredient
+
+class IndexView(generic.ListView):
+    template_name = "ingredients/index.html"
+    context_object_name = "ingredient_list"
+    
+    def get_queryset(self):
+        search = self.request.GET.get('search_param', '')
+
+        if search:
+            return Ingredient.objects.filter(FilterParam(name__icontains=search) | FilterParam(ean__icontains=search))
+        
+        return Ingredient.objects.all()

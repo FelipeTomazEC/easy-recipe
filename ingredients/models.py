8b50df01f1627d4772ceda33d2cfc1ed52f067b1
Felipe Tomaz <felipetomaz2206@gmail.com>
2024-10-20 13:00:42 -0300

    feat: define model Ingredient

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator, MinValueValidator
+from djmoney.models.fields import MoneyField
 
-# Create your models here.
+from enum import Enum
+
+class Unit(Enum):
+    GRAM = 'g'
+    KILOGRAM = 'Kg'
+    LITER = 'L'
+    CENTILITER = 'cl'
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    ean = models.CharField(primary_key=True, max_length=13, validators=[MinLengthValidator(13), MaxLengthValidator(13)])
+    standard_amount = models.BigIntegerField(validators=[MinValueValidator(0)])
+    standard_unit = models.CharField(max_length=20, choices=[(unit.name, unit.value) for unit in Unit])
+    cost_per_unit = MoneyField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+
